e3e09c700074ba1b789a2485de1d8ebad8d58ed7
Soheil Hoseini <soheil.hosseini78@gmail.com>
2021-11-17 19:51:22 +0330

    Menu for start

---
+++ Bridge Runner/Assets/Scripts/GMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI; //getting a reference to the "Highscore" text
+
+public class GMenu : MonoBehaviour
+{
+    public Text highscoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {                                        //gets the saved value from the file
+        highscoreText.text = "High Score : " + ((int)PlayerPrefs.GetFloat("Highscore")).ToString();//
+    }
+
+    //when we hit the "Start" button
+    public void ToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+}
